abb7fcc1de3e17272d72182bd29b7292a374df4b
CGrader <bgnrhck@hotmail.com>
2015-11-11 15:06:22 -0200

    Update InstructionDecoder.vhd

--- Source/Instruction/InstructionDecoder.vhd
+++ Source/Instruction/InstructionDecoder.vhd
@@ -32,35 +32,35 @@ begin
 	with instruction_byte select
    instruction.opcode <=
 		-- Memory Control instructions
-		LOAD when "00000000",
-		STORE when "00000001",
-		NOTCARRYBIT when "00000010",
+		LOAD			        when "00000000",
+		STORE 			 	when "00000001",
+		NOT_CARRY_BIT 			when "00000010",
 		-- Arithmetic Instructions 
-		ADD when "00010000",
-		SUBTRACT when "00010001",
-		MULTIPLY when "00010010",
-		DIVIDE when "00010011",
+		ADD 				when "00010000",
+		SUBTRACT 			when "00010001",
+		MULTIPLY 			when "00010010",
+		DIVIDE 				when "00010011",
 		-- Logic Instructions
-		BITWISEAND when "01000000",
-		BITWISEOR when "01000001",
-		BITWISENAND when "01000010",
-		BITWISENOR when "01000011",
-		BITWISEXOR when "01000100",
-		BITWISENOT when "01000101',
+		BITWISE_AND 			when "01000000",
+		BITWISE_OR 			when "01000001",
+		BITWISE_NAND 			when "01000010",
+		BITWISE_NOR 			when "01000011",
+		BITWISE_XOR 			when "01000100",
+		BITWISE_NOT 			when "01000101',
 		-- Comparision Instructions
-		GREATERTHAN when "00110000",
-		GREATEROREQUALTHAN when "00110001",
-		LESSTHAN when "00110010",
-		LESSOREQUALTHAN when "00110011",
-		EQUAL when "00110100",
-		NOTEQUAL when  "0011 0101",
+		GREATER_THAN 			when "00110000",
+		GREATER_OR_EQUAL_THAN 		when "00110001",
+		LESS_THAN 			when "00110010",
+		LESS_OR_EQUAL_THAN 		when "00110011",
+		EQUAL 				when "00110100",
+		NOT_EQUAL 			when "00110101",
 		-- Flow Control Instructions
-		JUMP when "00100000",
-		JUMPIFCARRY when "00100010",
+		JUMP 				when "00100000",
+		JUMP_IF_CARRY 			when "00100010",
 		-- Processor Control Instructions
-		HALT when "11111110",
-		RESET when "11111101",
-		HALT when others;
+		HAL				when "11111110",
+		RESET 				when "11111101",
+		HALT 				when others;
 	
 
 end InstructionDecoderImpl;
